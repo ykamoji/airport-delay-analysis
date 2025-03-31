fd25aa58d910c50498dcee4527075f056cdd8f8e
ykamoji <ykamoji@umass.edu>
2025-03-31 17:51:08 -0400

    Fixed title issues

--- js/stateDelays.js
+++ js/stateDelays.js
@@ -87,8 +87,8 @@ function transformRadius(x, inMin, inMax, outMin, outMax) {
     return (x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin;
 }
 
-function createSegmentedPieChart(data, colors, minVal, maxVal) {
-    let svg = $('#state-nightingale-chart');
+function createSegmentedPieChart(data, abbrList, colors, minVal, maxVal) {
+    let svg = $('#state-chart');
     let centerX = 200, centerY = 200;
     let numSegments = data.length;
     let totalAngle = Math.PI * 2;
@@ -98,7 +98,7 @@ function createSegmentedPieChart(data, colors, minVal, maxVal) {
         let startAngle = index * anglePerSegment;
         let radius = transformRadius(value, minVal, maxVal, 100, 200)
 
-        console.log(index, radius)
+        // console.log(index, radius)
 
         let x1 = centerX + radius * Math.cos(startAngle);
         let y1 = centerY + radius * Math.sin(startAngle);
@@ -113,7 +113,52 @@ function createSegmentedPieChart(data, colors, minVal, maxVal) {
         path.setAttribute("stroke", "whitesmoke");
         path.setAttribute("stroke-width", "3");
         svg.prepend(path);
+
+        const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+
+        // console.log(startAngle, startAngle + anglePerSegment)
+
+        let mid = (startAngle * 2 + anglePerSegment)/2
+        let deg = mid * (180 / Math.PI)
+        let text_x = centerX
+        let text_y = centerY
+
+        if(deg > 90 && deg < 270){
+            radius *= 1.2
+            deg = deg - 180
+        }else{
+            radius *= 1.05
+        }
+
+        text_x = centerX + radius * Math.cos(mid)
+        text_y = centerY + radius * Math.sin(mid)
+        text.setAttribute("transform", `rotate(${deg}, ${text_x}, ${text_y})`)
+        text.setAttribute("x", text_x+'')
+        text.setAttribute("y", text_y+'')
+
+        text.setAttribute("font-size", "11px")
+        text.textContent = abbrList[index]['abbrName']; // Display the original data value
+        svg.append(text);
     });
+}
+
+function getAirportAbbr(data){
+    let abbrList = []
+    data.forEach(d => {
+        let name = d['1'].split(':')[1].trim()
+        let abbrName = name
+            .replace(/-/g, '')
+            .split(' ')
+            .filter(s => s.length > 0)
+            .map(s => s[0].toUpperCase())
+            .join('')
+        abbrList.push({abbrName,name})
+    })
+
+    // console.log(abbrList)
+
+    return abbrList
+
 }
 
 function state_map_render(data, id){
@@ -134,7 +179,9 @@ function state_map_render(data, id){
         maxVal += 1000
     }
 
+    let abbrList = getAirportAbbr(filtered_data)
+
     let colorPalette = d3.scaleSequential([minVal, maxVal], d3.interpolateReds)
 
-    createSegmentedPieChart(dataValues, colorPalette, minVal, maxVal);
+    createSegmentedPieChart(dataValues, abbrList, colorPalette, minVal, maxVal);
 }
